7072c9aca6da8683b5adbb8a65e8f9de77db2f1c
YumYummity <103061664+YumYummity@users.noreply.github.com>
2025-09-13 15:33:54 -0400

    feat: update locale handling to json

--- locales/locale.py
+++ locales/locale.py
@@ -1,17 +1,38 @@
-import importlib
-from typing import Type, TypeVar, Dict
+import json
+from typing import Dict
 
-from .protocol import MessagesProtocol
 
-T = TypeVar("T", bound=MessagesProtocol)
+class Locale:
+    def __init__(self, data: dict, default: dict):
+        self._data = data
+        self._default = default
+
+    @property
+    def server_description(self) -> str:
+        return self._data["server_description"]
+
+    def item_not_found(self, item: str, name: str) -> str:
+        return self._data["item_not_found"].format(item=item, name=name)
+
+    def item_type_not_found(self, item: str) -> str:
+        return self._data["item_type_not_found"].format(item=item)
+
+    def items_not_found(self, item: str) -> str:
+        return self._data["items_not_found"].format(item=item)
+
+    def items_not_found_search(self, item: str) -> str:
+        return self._data["items_not_found_search"].format(item=item)
 
 
 class LocaleManager:
     def __init__(self, default_locale: str):
         self.default_locale = default_locale
-        self.locales: Dict[str, Type[MessagesProtocol]] = {}
+        self.locales: Dict[str, Locale] = {}
 
-    def load_locale(self, locale: str) -> Type[MessagesProtocol]:
+        self._default_locale = None
+        self._default_locale = self.load_locale("en", overwrite_default={})
+
+    def load_locale(self, locale: str, overwrite_default: dict = None) -> Locale:
         if locale == "zhs":
             locale = "zh-cn"
         elif locale == "zht":
@@ -19,15 +40,24 @@ class LocaleManager:
         if locale in self.locales:
             return self.locales[locale]
         try:
-            locale_module = importlib.import_module("locales.locales." + locale)
-            self.locales[locale] = locale_module.Messages
-            return locale_module.Messages
-        except (ModuleNotFoundError, AttributeError):
-            return self.load_locale(self.default_locale)
+            with open(f"locales/locales/{locale}.json", "r", encoding="utf8") as f:
+                d = json.load(f)
+            locale_class = Locale(
+                d,
+                (
+                    overwrite_default
+                    if overwrite_default != None
+                    else self._default_locale._data
+                ),
+            )
+            self.locales[locale] = locale_class
+            return locale_class
+        except FileNotFoundError:
+            return self._default_locale
 
-    def get_messages(self, locale: str) -> Type[MessagesProtocol]:
-        messages_class = self.load_locale(locale)
-        return messages_class
+    def get_messages(self, locale: str) -> Locale:
+        locale_class = self.load_locale(locale)
+        return locale_class
 
 
 Locale = LocaleManager(default_locale="en")
--- locales/protocol.py
+++
@@ -1,9 +0,0 @@
-from typing import Protocol, Callable
-
-
-class MessagesProtocol(Protocol):
-    server_description: str
-    item_not_found: Callable
-    item_type_not_found: Callable
-    items_not_found: Callable
-    items_not_found_search: Callable
