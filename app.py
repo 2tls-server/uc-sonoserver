596fad30e5c7e61509b75f09bd4cc486da6132fe
YumYummity <103061664+YumYummity@users.noreply.github.com>
2025-09-20 10:57:26 -0400

    fix: disable logging

--- app.py
+++ app.py
@@ -214,10 +214,12 @@ app.add_event_handler("startup", startup_event)
 
 async def start_fastapi(args):
     config_server = uvicorn.Config(
-        "app:app", host="0.0.0.0", port=config["server"]["port"], workers=11
+        "app:app",
+        host="0.0.0.0",
+        port=config["server"]["port"],
+        workers=11,
+        access_log=False,
     )
-    if not debug:
-        config_server.log_level = "critical"
     server = uvicorn.Server(config_server)
     await server.serve()
 
